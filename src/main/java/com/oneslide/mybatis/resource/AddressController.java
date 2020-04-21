25aa7e178ffdcf89a62a59e5d0cf864f87bc5c7c
Your Name <you@example.com>
2020-04-21 14:47:07 +0800

    final fix

--- src/main/java/com/oneslide/mybatis/resource/AddressController.java
+++ src/main/java/com/oneslide/mybatis/resource/AddressController.java
@@ -3,6 +3,8 @@ package com.oneslide.mybatis.resource;
 import com.oneslide.mybatis.mapper.AddressMapper;
 import com.oneslide.mybatis.model.Address;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,12 +12,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 @RestController
-@RequestMapping("address")
+@RequestMapping("/address")
 public class AddressController {
 
     @Autowired
     AddressMapper mapper;
 
+    @Autowired
+    @Qualifier("mybatis-auto-addr")
+    com.oneslide.mybatis.mybatis.mapper.AddressMapper autoMapper;
     @GetMapping("/basicSearch")
     public List<Address> searchAddress(@RequestParam String street){
         Map<String,Object> map =new HashMap<>();
@@ -25,5 +30,11 @@ public class AddressController {
         map.put("idList",list);
         List<Address> lista =mapper.elasticSearchAddressForeach(map);
         return lista;
+    }
+
+    @PostMapping("/add")
+    public String insertAddress(@RequestBody com.oneslide.mybatis.mybatis.model.Address address){
+         int n=autoMapper.insert(address);
+         return n==1?"ok":"failed";
     }
 }
